0b4c5372e62af59041ab4f25841e30543146b26c
pashka11 <baks2964@gmail.com>
2017-06-23 10:21:10 +0300

    fake seats

--- src/main/java/nimrodpasha/cinema/WebApi/SeatsResource.java
+++ src/main/java/nimrodpasha/cinema/WebApi/SeatsResource.java
@@ -9,7 +9,8 @@ import org.bson.Document;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
+import nimrodpasha.cinema.objects.Seat;
+import static nimrodpasha.cinema.utils.Parameters.*;
 /**
  * Created by Yuval on 18-Mar-17.
  * This class handles the seat related HTTP requests
@@ -19,19 +20,28 @@ import javax.ws.rs.core.Response;
 @Produces(MediaType.APPLICATION_JSON)
 public class SeatsResource {
 
-
-    /**
-     * @param showInstanceId to return
-     * @return requested show instance seats
-     */
     @GET
     @Path("/{showInstanceId}")
-    public Response getShowInstanceSeats(@PathParam("showInstanceId") String showInstanceId) {
-        Crud crud =new ShowInstanceDao();
-        Document document = crud.read(showInstanceId);
-        if (document!=null && document.get(Parameters.SHOW_INSTANCE_SEATS)!=null){
-            return Response.status(Response.Status.OK).entity(JSON.serialize(document.get(Parameters.SHOW_INSTANCE_SEATS))).build();
-        }
-        return Response.status(Response.Status.BAD_REQUEST).entity(Parameters.INVALID_SHOW_INSTANCE_ID).build();
+    public String getMovieInstanceSeats(@PathParam("showInstanceId") String showInstanceId) {
+
+        return ("row 1: " +" 1 "+" 2 "+" 3 "+" 4 " +"\n" + "row 2: "+ " 1 " + " # " + " # " + " 4 " + "\n");
+
     }
+
+
+//
+//    /**
+//     * @param showInstanceId to return
+//     * @return requested show instance seats
+//     */
+//    @GET
+//    @Path("/{showInstanceId}")
+//    public Response getShowInstanceSeats(@PathParam("showInstanceId") String showInstanceId) {
+//        Crud crud =new ShowInstanceDao();
+//        Document document = crud.read(showInstanceId);
+//        if (document!=null && document.get(Parameters.SHOW_INSTANCE_SEATS)!=null){
+//            return Response.status(Response.Status.OK).entity(JSON.serialize(document.get(Parameters.SHOW_INSTANCE_SEATS))).build();
+//        }
+//        return Response.status(Response.Status.BAD_REQUEST).entity(Parameters.INVALID_SHOW_INSTANCE_ID).build();
+//    }
 }
